2b27e900bba0f768bf6c709481a4fe4b2826dfdf
Kyle Siegel <kyle.siegel1@gmail.com>
2016-08-09 23:24:24 -0600

    finished evaluation and validation modules?

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -22,10 +22,31 @@ data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") 
 features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
+sort_keys='../tools/python2_lesson14_keys.pkl'
 labels, features = targetFeatureSplit(data)
 
+from sklearn.cross_validation import train_test_split
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.30,
+                                                                             random_state=42)
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 
-### your code goes here 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(labels_test, pred)
+prec = precision_score(labels_test, pred)
+rec = recall_score(labels_test, pred)
+
+print "Accuracy:", acc
+print "Predicted POI's:", sum(pred)
+print "Size of test set:", len(pred)
+print "True positives:", sum([pred[i]==labels_test[i] for i in range(0, len(pred)) if pred[i]])
+print "Precision:", prec
+print "Recall:", rec
+
 
 
--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -25,8 +25,19 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+from sklearn.cross_validation import train_test_split
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.30,
+                                                                             random_state=42)
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 
-### it's all yours from here forward!  
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(labels_test, pred)
+print acc
 
 
